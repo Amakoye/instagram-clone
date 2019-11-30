c3c9cf30864ed12bc3785641e59a667e64991229
Amakoye <charlesamakoye@gmail.com>
2019-11-30 11:20:44 +0300

    added new field username for register

--- instagram-clone-app/resources/views/home.blade.php
+++ instagram-clone-app/resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row justify-content-center mx-auto">
+        <div class="col-md-3 pt-5">
+            <img src="/img/pp.jfif" class="rounded-circle" style="max-height: 200px;">
+        </div>
+        <div class="col-md-9 pt-5">
+            <div><h1>Charles Amakoye</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">charlesamakoye.com</div>
+            <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, libero dolorum porro velit dicta quidem?</div>
+            <div class=""><a href="#">www.charlesweezy.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-md-4">
+            <img src="/img/pp.jfif" class="w-100" alt="">
+        </div>
+        <div class="col-md-4">
+            <img src="/img/pp.jfif" class="w-100" alt="">
+        </div>
+        <div class="col-md-4">
+            <img src="/img/pp.jfif" class="w-100" alt="">
         </div>
     </div>
 </div>
